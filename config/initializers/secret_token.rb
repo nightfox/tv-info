98cbe89c9e11e7cd1348cabd43ead88fb742ecf3
Anirvan Mandal <anirvan.mandal@gmail.com>
2013-07-16 20:07:21 +0530

    Initial commit after setting up repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TvInfo::Application.config.secret_token = 'd7c3ad59d104590476dd560cab8cb18ee5f931a24e5ff02aa04a698a6d4cd53d8b47e06843c59e494750bc7539fbbee6864001dd56ae78c570911771138ef7b5'
